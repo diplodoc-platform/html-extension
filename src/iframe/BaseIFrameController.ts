1ea6e1b2364e7ff8cf3ea4878121bfeae3e3731d
Andrey Volynkin <avol-v@yandex-team.ru>
2025-02-18 15:49:31 +0100

    Remove old comments

--- src/iframe/BaseIFrameController.ts
+++ src/iframe/BaseIFrameController.ts
@@ -20,14 +20,12 @@ export abstract class BaseIFrameController extends Disposable {
     setClassNames = (classNames: string[] | undefined = []) => {
         updateClassNames(this.domContainer, classNames, this.classNames);
 
-        // update this._classNames to the new classNames
         this.classNames = classNames;
     };
 
     setStyles = (styles: Record<string, string> | undefined = {}) => {
         updateStyles(this.domContainer, styles, this.styles);
 
-        // update this._styles to the new styles
         this.styles = styles;
     };
 
@@ -35,8 +33,6 @@ export abstract class BaseIFrameController extends Disposable {
         const fragment = globalThis.document.createRange().createContextualFragment(htmlString);
 
         this.domContainer.replaceChildren(fragment);
-
-        // TODO: should we reinitialize Observer?
     };
 
     setBaseTarget = (value: string) => {
