b155de824dff637f6130267391f694e857cbc992
Sergey Makhnatkin <makhnatkin@yandex-team.ru>
2026-04-08 13:06:01 +0200

    fix(sanitize): block iframe in default body sanitizer (#97)

--- src/plugin/index.ts
+++ src/plugin/index.ts
@@ -2,3 +2,9 @@ export type {BaseTarget, StylesObject, HTMLRuntimeConfig, EmbeddingMode} from '.
 export type {PluginOptions} from './transform';
 export {getStyles} from './utils';
 export {transform} from './transform';
+export type {YfmHtmlBlockBuildOptions} from '../utils/sanitize';
+export {
+    buildYfmHtmlBlockOptions,
+    yfmHtmlBlockOptions,
+    htmlBlockDefaultSanitizer,
+} from '../utils/sanitize';
